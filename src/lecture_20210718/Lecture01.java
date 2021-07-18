496a109c958bf5fd927bb33b21fa39cd0d53182d
Your Name <you@example.com>
2021-07-18 12:36:21 +0900

    initial commit

---
+++ src/lecture_20210718/Lecture01.java
@@ -0,0 +1,18 @@
+package lecture_20210718;
+
+import java.util.Random;
+
+public class Lecture01 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int sum = 0;
+        for(int i = 0;i<5;i++){
+            sum+=getRandomInt(1,6);
+        }
+        System.out.println("획득: $ "+sum);
+        System.out.println("손익: $ "+(sum-18));
+    }
+    public static int getRandomInt(double min,double max) {
+        return (int) (Math.random() * 6 + 1);
+    }
+}
