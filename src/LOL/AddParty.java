40871c49a9efcc9df04dcdaa77e375c8c408576b
bluefire2323 <cyh1823@gmail.com>
2021-11-28 23:13:52 +0900

    20211128

--- src/LOL/AddParty.java
+++ src/LOL/AddParty.java
@@ -1,8 +1,6 @@
 package LOL;
 
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddParty {
     void partyMatch(int partyNum){
@@ -17,9 +15,7 @@ public class AddParty {
         }
         int position [] = {0,1,2,3,4};
 
-        for (int i = 0 ; i < 5 ; i++){
-            System.out.println(position[i]);
-        }
+
 
     }
 }
